605e6385c7a2d4f519ef03f4b1f462a43a6dde58
Matt <109008943+SirNewblet@users.noreply.github.com>
2025-07-29 22:41:02 -0500

    Firsrt portion of implementing the Level Editor for the game. The level editor will implement 2 views (one for "inventory" of entities to place, and one for the actual world).

---
+++ Scene_LevelEditor.cpp
@@ -0,0 +1,304 @@
+#include "Scene_LevelEditor.h"
+#include "Physics.h"
+#include "Assets.h"
+#include "GameEngine.h"
+#include "Components.h"
+#include "Action.h"
+
+#include <iostream>
+#include <fstream>
+
+bool isInside(Vec2 pos, Entity e)
+{
+	auto ePos = e.getComponent<CTransform>().pos;
+	auto size = e.getComponent<CAnimation>().animation.getSize();
+	float dx = fabs(pos.x - ePos.x);
+	float dy = fabs(pos.y - ePos.y);
+
+	return ((dx <= size.x / 2) && (dy <= size.y / 2));
+}
+
+Scene_LevelEditor::Scene_LevelEditor(GameEngine* gameEngine, const std::string& levelPath) :
+	Scene(gameEngine),
+	m_levelPath(levelPath),
+	m_gridText(m_game->assets().getFont("Sooky"))
+{
+	init(m_levelPath);
+}
+
+void Scene_LevelEditor::init(const std::string& levelPath)
+{
+	registerAction(sf::Keyboard::Key::Escape, "QUIT");
+	registerAction(sf::Keyboard::Key::T, "TOGGLE_TEXTURE");				// Toggle drawing (T)extures
+	registerAction(sf::Keyboard::Key::G, "TOGGLE_GRID");					// Toggle drawing (G)rid
+
+	registerAction(sf::Keyboard::Key::Space, "ASCEND");
+	registerAction(sf::Keyboard::Key::A, "LEFT");
+	registerAction(sf::Keyboard::Key::D, "RIGHT");
+	registerAction(sf::Keyboard::Key::S, "DESCEND");
+
+	m_gridText.setCharacterSize(24);
+
+	loadLevel(levelPath);
+}
+
+// IMPORTANT: Always add the CAnimation component first so that gridToMidPixel can compute correctly
+Vec2 Scene_LevelEditor::gridToMidPixel(float gridX, float gridY, Entity entity)
+{
+	//			This function takes in a grid (x, y) position and an Entity
+	//			Return a Vec2 indicating where the CENTER position of the Entity should be
+	//			You must use the Entity's Animation size to position it correctly
+	//			The size of the grid width and height is stored in m_gridSize.x and m_gridSize.y
+	//			The bottom-left corner of the Animation should align with the bottom left of the grid cell
+
+	float x = 0.0f, y = 0.0f;
+	x = (gridX * m_gridSize.x) + (entity.getComponent<CAnimation>().animation.getSize().x / 2);
+	y = m_game->window().getSize().y - (gridY * m_gridSize.y) - (entity.getComponent<CAnimation>().animation.getSize().y / 2 * entity.getComponent<CTransform>().scale.y);
+
+	return Vec2(x, y);
+}
+
+Vec2 Scene_LevelEditor::windowToWorld(const Vec2& windowPos) const
+{
+	auto view = m_game->window().getView();
+
+	float wx = view.getCenter().x - (m_game->window().getSize().x / 2);
+	float wy = view.getCenter().y - (m_game->window().getSize().y / 2);
+
+	return Vec2(windowPos.x + wx, windowPos.y + wy);
+}
+
+void Scene_LevelEditor::loadLevel(const std::string& filename)
+{
+	// Reset the entity manager every time we load a level
+	m_entityManager = EntityManager();
+
+	std::ifstream fin(filename);
+	std::string item = "";
+
+	while (fin >> item)
+	{
+		if (item == "Tile" || item == "Decoration" || item == "Ladder")
+		{
+
+			int tileGX = 0, tileGY = 0;
+			std::string name;
+
+			fin >> name >> tileGX >> tileGY;
+
+			auto entity = m_entityManager.addEntity(item);
+			entity.addComponent<CAnimation>(m_game->assets().getAnimation(name), true);
+			entity.addComponent<CTransform>();
+			entity.addComponent<CDraggable>();
+			entity.getComponent<CTransform>().pos = gridToMidPixel(tileGX, tileGY, entity);
+		}
+		else if (item == "Enemy")
+		{
+			int tileGX = 0, tileGY = 0, damage = 0;
+			std::string name;
+
+			fin >> name >> tileGX >> tileGY >> damage;
+
+			auto entity = m_entityManager.addEntity(item);
+			entity.addComponent<CAnimation>(m_game->assets().getAnimation(name), true);
+			entity.addComponent<CTransform>();
+			entity.addComponent<CDraggable>();
+			entity.getComponent<CTransform>().pos = gridToMidPixel(tileGX, tileGY, enemy);
+		}
+		else if (item == "Player")
+		{
+			fin
+				>> m_playerConfig.gridX
+				>> m_playerConfig.gridY
+				>> m_playerConfig.collisionX
+				>> m_playerConfig.collisionY
+				>> m_playerConfig.speedX
+				>> m_playerConfig.speedY
+				>> m_playerConfig.maxSpeed
+				>> m_playerConfig.gravity
+				>> m_playerConfig.WEAPON;
+		}
+	}
+
+	spawnPlayer();
+
+
+	//      NOTE:
+	//------  THIS IS INCREDIBLY IMPORTANT PLEASE READ THIS EXAMPLE  ----------
+	//		Components are now returned as references rather than pointers. If you
+	//		do not specify a reference variable type, it will COPY the component
+	//		Here is an example:
+	//		
+	//		This will COPY the transform into the variable "transform1" - it is INCORRECT
+	//		Any changes you make to transform1 will not be changed inside the entity
+	//		auto transform1 = entity->get<CTransform>()
+	//
+	//		This will REFERENCE the transform with the variable "transform2" - it is CORRECT
+	//		Now any changes you make to transform2 will be changed inside the entity
+	//		auto& transform2 = entity->get<CTransform>()
+}
+
+void Scene_LevelEditor::spawnPlayer()
+{
+	m_player = m_entityManager.addEntity("Player");
+	m_player.addComponent<CAnimation>(m_game->assets().getAnimation("PlayerStand"), true);
+	m_player.addComponent<CTransform>(Vec2(gridToMidPixel(m_playerConfig.gridX, m_playerConfig.gridY, m_player)));
+	m_player.addComponent<CDraggable>();
+	m_player.addComponent<CHealth>();
+}
+
+void Scene_LevelEditor::update()
+{
+	m_entityManager.update();
+
+	// TODO: Implement pause functionality
+	sDragAndDrop();
+	sMovement();
+	sCamera();
+	sRender();
+
+	m_currentFrame++;
+}
+
+void Scene_LevelEditor::sDragAndDrop()
+{
+	for (auto e : m_entityManager.getEntities())
+	{
+		if (e.hasComponent<CDraggable>() && e.getComponent<CDraggable>().dragging)
+		{
+			e.getComponent<CTransform>().pos = windowToWorld(m_mPos);
+		}
+	}
+}
+
+void Scene_LevelEditor::sCamera()
+{
+	// TODO: Keep Camera on player unless player runs left / falls down a hole / enters a gate
+	// Set the viewport of the window to be centered on the player if it's far enough right
+	auto& pPos = m_player.getComponent<CTransform>().pos;
+	float windowCenterX = std::max(m_game->window().getSize().x / 2.0f, pPos.x);
+	float windowCenterY = std::min(m_game->window().getSize().y / 2.0f, pPos.y);
+	sf::View view = m_game->window().getView();
+	view.setCenter({ windowCenterX, windowCenterY });
+	m_game->window().setView(view);
+}
+
+void Scene_LevelEditor::sMovement()
+{
+
+
+}
+
+void Scene_LevelEditor::sDoAction(const Action& action)
+{
+	// DO NOT MODIFY PLAYER MOVEMENT HERE - ONLY FOR USING ACTION TO SET CInput COMPONENT
+	if (action.type() == "START")
+	{
+		if (action.name() == "TOGGLE_TEXTURE") { m_drawTextures = !m_drawTextures; }
+		if (action.name() == "TOGGLE_GRID") { m_drawGrid = !m_drawGrid; }
+		if (action.name() == "QUIT") { onEnd(); }
+		if (action.name() == "JUMP") { m_player.getComponent<CInput>().jump = true; }
+		if (action.name() == "CROUCH") { m_player.getComponent<CInput>().down = true; }
+		if (action.name() == "LEFT") { m_player.getComponent<CInput>().left = true; }
+		if (action.name() == "RIGHT") { m_player.getComponent<CInput>().right = true; }
+		if (action.name() == "LEFT_CLICK")
+		{
+			Vec2 worldPos = windowToWorld(action.pos());
+			//std::cout << "Mouse clicked at: " << worldPos.x << ", " << worldPos.y << std::endl;
+			for (auto e : m_entityManager.getEntities())
+			{
+				if (e.hasComponent<CDraggable>() && isInside(worldPos, e))
+				{
+					std::cout << "CLICKED ON ENTITY: " << e.getComponent<CAnimation>().animation.getName() << std::endl;
+					e.getComponent<CDraggable>().dragging = !e.getComponent<CDraggable>().dragging;
+					if (!e.getComponent<CDraggable>().dragging)
+					{
+						// put entity into the correct grid location
+						 
+					}
+				}
+			}
+		}
+		if (action.name() == "MOUSE_MOVE")
+		{
+			m_mPos = action.pos();
+			m_mouseShape.setPosition({ m_mPos.x, m_mPos.y });
+		}
+	}
+	else if (action.type() == "END")
+	{
+		if (action.name() == "CLIMB") { m_player.getComponent<CInput>().up = false; }
+		if (action.name() == "JUMP") { m_player.getComponent<CInput>().jump = false; }
+		if (action.name() == "CROUCH") { m_player.getComponent<CInput>().down = false; }
+		if (action.name() == "LEFT") { m_player.getComponent<CInput>().left = false; }
+		if (action.name() == "RIGHT") { m_player.getComponent<CInput>().right = false; }
+	}
+}
+
+void Scene_LevelEditor::sRender()
+{
+	// Color the background darker so you know that the game is paused
+	if (!m_paused) { m_game->window().clear(sf::Color(0xa83e75)); }
+	else { m_game->window().clear(sf::Color(0xa83ea8)); }
+
+	// Draw all Entity textures + animations
+	if (m_drawTextures)
+	{
+		for (auto e : m_entityManager.getEntities())
+		{
+			auto& transform = e.getComponent<CTransform>();
+
+			if (e.hasComponent<CAnimation>())
+			{
+				auto& animation = e.getComponent<CAnimation>().animation;
+				animation.getSprite().setRotation(sf::degrees(transform.angle));
+				animation.getSprite().setPosition({ transform.pos.x, transform.pos.y });
+				animation.getSprite().setScale({ transform.scale.x, transform.scale.y });
+				m_game->window().draw(animation.getSprite());
+			}
+		}
+	}
+
+	// Draw the grid so that students can easily debug
+	if (m_drawGrid)
+	{
+		float leftX = m_game->window().getView().getCenter().x - width() / 2;
+		float rightX = leftX + width() + m_gridSize.x;
+		float nextGridX = leftX - ((int)leftX % (int)m_gridSize.x);
+
+		for (float x = nextGridX; x < rightX; x += m_gridSize.x)
+		{
+			drawLine(Vec2(x, 0.0f), Vec2(x, (float)height()));
+		}
+
+		for (float y = 0; y < height(); y += m_gridSize.y)
+		{
+			drawLine(Vec2(leftX, height() - y), Vec2(rightX, height() - y));
+
+			for (float x = nextGridX; x < rightX; x += m_gridSize.x)
+			{
+				std::string xCell = std::to_string((int)x / (int)m_gridSize.x);
+				std::string yCell = std::to_string((int)y / (int)m_gridSize.y);
+				m_gridText.setString("(" + xCell + "," + yCell + ")");
+				m_gridText.setPosition({ x + 3, height() - y - m_gridSize.y + 2 });
+				m_game->window().draw(m_gridText);
+			}
+		}
+	}
+	m_mouseShape.setFillColor(sf::Color(255, 0, 0));
+	m_mouseShape.setRadius(4);
+	m_mouseShape.setOrigin({ 2, 2 });
+	Vec2 worldPos = windowToWorld(m_mPos);
+	m_mouseShape.setPosition({ worldPos.x, worldPos.y });
+	m_game->window().draw(m_mouseShape);
+	m_currentFrame++;
+}
+
+void Scene_LevelEditor::onEnd()
+{
+	m_hasEnded = true;
+	sf::View view = m_game->window().getView();
+	view.setCenter({ m_game->window().getSize().x / 2.0f, m_game->window().getSize().y / 2.0f });
+	m_game->window().setView(view);
+	m_game->changeScene("MENU", nullptr, true);
+}
---
+++ Scene_LevelEditor.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "Scene.h"
+#include "EntityManager.h"
+
+#include <map>
+#include <memory>
+
+class Scene_LevelEditor : public Scene
+{
+
+	struct PlayerConfig
+	{
+		float gridX = 0, gridY = 0, collisionX = 0, collisionY = 0, speedX = 0, speedY = 0, maxSpeed = 0, gravity = 0;
+		std::string WEAPON;
+	};
+
+protected:
+	Entity								m_player;
+	PlayerConfig						m_playerConfig;
+	std::string							m_levelPath;
+	std::string							m_lastAction;
+	bool								m_drawTextures = true;
+	bool								m_drawGrid = false;
+	const Vec2							m_gridSize = { 64, 64 };
+	sf::Text							m_gridText;
+
+	Vec2								m_mPos;
+	sf::CircleShape						m_mouseShape;
+
+	Vec2 gridToMidPixel(float gridX, float gridY, Entity);
+	Vec2 windowToWorld(const Vec2& windowPos) const;
+
+	void init(const std::string& levelPath);
+	void loadLevel(const std::string& filename);
+	void onEnd();
+	void update();
+
+	void spawnPlayer();
+	void spawnBullet(Entity entity);
+
+	void sDragAndDrop();
+	void sMovement();
+	void sDoAction(const Action& action);
+	void sCamera();
+
+public:
+	Scene_LevelEditor(GameEngine* gameEngine, const std::string& levelPath);
+
+	void sRender();
+};
